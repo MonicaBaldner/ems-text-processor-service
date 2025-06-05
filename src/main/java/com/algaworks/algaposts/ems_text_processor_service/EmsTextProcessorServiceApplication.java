232b81cc2b4b043fd7b9847f08e112b1671ad55b
Mônica Baldner <baldnermonica@gmail.com>
2025-06-05 19:49:17 -0300

    atualizando configurações

---
+++ src/main/java/com/algaworks/algaposts/ems_text_processor_service/EmsTextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaposts.ems_text_processor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsTextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsTextProcessorServiceApplication.class, args);
+	}
+
+}
